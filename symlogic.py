5dcfa8536b6598134250febffa9c6094971c5a5a
David-OConnor <david.alan.oconnor@gmail.com>
2017-08-30 03:27:45 -0400

    first commit

---
+++ symlogic.py
@@ -0,0 +1,119 @@
+
+POLITE = [
+    "please",
+    "thanks",
+    "thank you",
+    "sorry",
+    "you're welcome",
+
+]
+
+IMPOLITE = [
+    "stupid",
+    "stupidest",
+    "retarded",
+    "fucking",
+    "lmfao",
+]
+
+
+NARCISSISTIC = [
+    'I',
+    'me',
+
+]
+
+
+ABSOLUTE = [
+    "nothing",
+    "everything",
+    "none",
+    "all",
+    "always",
+    "never",
+
+]
+
+
+SUFFIXES = [
+    "ing",
+    "s",
+    "ed",
+
+]
+
+
+CONFIDENT = [
+    'guarantee',
+    'sure',
+    "of course",
+]
+
+WEASELS = [
+    "suggests"
+]
+
+
+VAPID = [
+    "look at",
+
+]
+
+
+STRONG = [
+    "destroy",
+    "destroying",
+    "so bad",
+]
+
+
+REPLACEMENTS = [
+    ("i think", ""),
+    ("necessarily", ""),
+    ("so maybe", ""),
+    ("aren't that", "aren't"),
+    ("maybe also", ""),
+    ("actually", ""),
+    ("so", ""),
+    ("I guarantee", ""),
+    ("a lot", ""),
+    ("really", ""),
+    ("take a look at", "look at"),
+    ("in fact", ""),
+
+]
+
+PASSIVE_HINTS = [
+    "were",
+    "have been",
+]
+
+PROBABILISTIC = [
+    "maybe",
+    "probably",
+    "i think",
+    "mostly",
+
+]
+
+QUALIFIERS = [
+    "unfortunately",
+    "ever",
+    "mostly",
+    "highly",
+    "always",
+    "probably",
+    "main",
+    "also",
+    "too",
+    "that",
+    "so",
+    "really",
+    "even",
+]
+
+
+
+
+def decode(raw_text: str) -> str:
+    pass
